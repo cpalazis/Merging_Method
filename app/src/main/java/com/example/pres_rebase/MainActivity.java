83a16d8ddd3603c6f42f2860d455b827b5784e34
cpalazis <cpalazis@yahoo.com>
2022-11-20 19:01:53 +0200

    This is the second commit of Branch2

--- app/src/main/java/com/example/pres_rebase/MainActivity.java
+++ app/src/main/java/com/example/pres_rebase/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("Αυτό είναι το Branch2");
+        System.out.println("Αυτό είναι το δεύτερο commit του Branch2");
     }
 }
